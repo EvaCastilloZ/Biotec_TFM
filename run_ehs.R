ac56460c8a9cf247829a637a69f874afea20a67d
EvaCastilloZ <102521069+EvaCastilloZ@users.noreply.github.com>
2022-07-12 18:41:20 +0200

    Add files via upload

--- run_ehs.R
+++ run_ehs.R
@@ -1,22 +1,26 @@
-# Specify work directory
-setwd ("/(Specify work directory)
+ setwd ("/home/eva/Escritorio/")
  # Code to load target file
- read.csv("target.txt")
-tableoftarget <- read.csv("target.txt", sep="\t")
+ read.csv("TARGET.txt")
+tableoftarget <- read.csv("TARGET.txt", sep="\t")
 # Code to load counts file
-read.table("counts.txt")
-tableofcountswrn <- read.table("counts.txt", sep="\t", row.names=1, header=TRUE)
-# Code to run expression analysis (and coexpression)
+read.table("COUNTS.txt")
+tableofcountswrn <- read.table("COUNTS.txt", sep="\t", row.names=1, header=TRUE)
+# Code to run expression analysis
 library(ExpHunterSuite)
-DEAcomplete <- main_degenes_Hunter(raw=tableofcountswrn, target=tableoftarget, modules="DNLE", output_files = "RESULTS")
-DEAcompleteCOexp <- main_degenes_Hunter(raw=tableofcountswrn, target=tableoftarget, modules="DNLEW", output_files = "RESULTS")
+#DEAcomplete <- main_degenes_Hunter(raw=tableofcountswrn, target=tableoftarget, modules="DNLE", output_files = "RESULTS")
+
+# TO RUN WGCNA too
+DEAcompleteCOexp <- main_degenes_Hunter(raw=tableofcountswrn, target=tableoftarget, modules="DW", output_files = "RESULTS")
 # Code to make a report on the expression analysis
+#write_expression_report(exp_results=DEAcomplete) 
 write_expression_report(exp_results=DEAcompleteCOexp) 
 # Code to run functional analysis
-Functionalanalysis <- functional_hunter(DEAcomplete, 'Mouse', func_annot_db = "gKR", GO_subont = "BMC", analysis_type= "o")
-FunctionalanalysisCOexp <- functional_hunter(DEAcompleteCOexp, 'Mouse', func_annot_db = "gKR", GO_subont = "BMC", analysis_type= "o")
+#Functionalanalysis <- main_functional_hunter(DEAcomplete, 'Human')
+FunctionalanalysisCOexp <- main_functional_hunter(DEAcompleteCOexp, 'Human')
 # Code to make a report on the functional analysis
 print(wd())
 write_enrich_files(func_results=Functionalanalysis)
 write_functional_report(hunter_results=DEAcomplete, func_results=Functionalanalysis)
+
+
 > write_functional_report(hunter_results=DEAcompleteCOexp, func_results=FunctionalanalysisCOexp)
