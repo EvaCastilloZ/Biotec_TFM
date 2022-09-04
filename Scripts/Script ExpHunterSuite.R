31adf3716328ea19a7e1ffb43d6d1a614ff198dc
EvaCastilloZ <ecastillozafra@uma.es>
2022-09-04 13:44:49 +0200

    little modifications to script

--- Scripts/Script ExpHunterSuite.R
+++ Scripts/Script ExpHunterSuite.R
@@ -40,33 +40,28 @@ table_of_target <- read.csv("../Data/target.txt", sep="\t")
 # Code to load counts file
 counts_no_rownames <- read.table("../Data/counts_modified.txt", sep="\t", row.names=1, header=TRUE)
 
-
 # To run coexpression analysis (with WGCNA)
-dea_coexpression <- main_degenes_Hunter(raw=counts_no_rownames,
+hunter_results <- main_degenes_Hunter(raw=counts_no_rownames,
                                         target=table_of_target, 
                                         modules="DW", 
                                         output_files = "RESULTS")
                                         
-write_expression_report(exp_results=dea_coexpression) 
-  
-#save(dea_coexpression, file="../Data/dea_coexpression.RData")
-load("dea_coexpression.RData")
-
-A <-  dea_coexpression$DE_all_genes
-A[A == "NOT_DEG"] <- "PREVALENT_DEG"
+write_expression_report(exp_results=hunter_results) 
+#save(dea_coexpression, file="../Data/hunter_results.RData")
+load("hunter_results.RData")
+ 
+hunter_results$DE_all_genes[hunter_results$DE_all_genes == "NOT_DEG"] <- "PREVALENT_DEG"
+#save(hunter_results, file="../Data/hunter_results_genetagmodified.RData")
+load("hunter_results_genetagmodified.RData")
 
 # Code to run functional analysis
 #functional_analysis <- main_functional_hunter(dea_4_modules, 'Human', enrich_dbs = c("MF", "BP","Reactome"), enrich_methods = "ORA")
-coexpression_functional_analysis <- main_functional_hunter(A,
-                                                           'Human', 
-                                                           enrich_dbs = c("MF","BP","Reactome"), 
+func_results <- main_functional_hunter(hunter_results, 'Human', 
+                                                           enrich_dbs = c("MF","BP","CC"), 
                                                            enrich_methods = "ORA",
                                                            input_gene_id = "ENTREZID" )
-print(wd())
-write_enrich_files(func_results=coexpression_functional_analysis)
-write_functional_report(hunter_results=dea_4_modules,
-                        func_results=functional_analysis)
-write_functional_report(hunter_results=dea_coexpression,
-                        func_results=coexpression_functional_analysis)
+write_enrich_files(func_results)
+write_functional_report(hunter_results=hunter_results,
+                        func_results=func_results)
 
 
